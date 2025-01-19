e308c4246ad146a240a0fcf8e16ce7e946b7dda1
Hayden Eubanks <haydenubanx@gmail.com>
2025-01-19 20:30:33 +0000

    Uplifted to JUnit 5

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -4,19 +4,24 @@ import Context.TestContext;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"StepDefinitions", "Hooks"},
-        tags = "@Portfolio",
-        plugin = {
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-                "pretty",
-                "json:target/cucumber-reports/cucumber.json",
-                "html:target/cucumber-reports/cucumber.html"
-        })
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-reports/cucumber.html,json:target/cucumber-reports/cucumber.json,io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+@ConfigurationParameter(key = "cucumber.options", value = "--threads 4")
+@ConfigurationParameter(key = "cucumber.glue", value = "classpath:StepDefinitions,classpath:Hooks")
+@ConfigurationParameter(key = "cucumber.tags", value = "@Portfolio")
+@Execution(ExecutionMode.CONCURRENT)
 public class TestRunner {
 
 
